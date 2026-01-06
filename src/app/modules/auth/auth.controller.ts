0caab5cedbcc82682ce8235945b96d5019fd08d4
Arman3747 <dailyarman7@gmail.com>
2026-01-06 19:41:39 +1100

    set-password added in Auths route

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -133,6 +133,43 @@ const resetPassword = catchAsync(
   }
 );
 
+const setPassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user as JwtPayload;
+    const { password } = req.body;
+
+    await AuthServices.setPassword(decodedToken.userId, password);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Password Changed Successfully !",
+      data: null,
+    });
+  }
+);
+
+const changePassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+
+    await AuthServices.changePassword(
+      oldPassword,
+      newPassword,
+      decodedToken as JwtPayload
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Password Changed Successfully !",
+      data: null,
+    });
+  }
+);
+
 const googleCallbackController = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     let redirectTo = req.query.state ? (req.query.state as string) : "";
@@ -171,5 +208,7 @@ export const AuthControllers = {
   getNewAccessToken,
   logout,
   resetPassword,
+  setPassword,
+  changePassword,
   googleCallbackController,
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -5,11 +5,10 @@ import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 
-import {
-  creteNewAccessTokenWithRefreshToken,
-} from "../../utils/userTokens";
+import { creteNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
+import { IAuthProvider } from "../user/user.interface";
 
 // const credentialsLogin = async (payload: Partial<IUser>) => {
 //   const { email, password } = payload;
@@ -75,7 +74,67 @@ const getNewAccessToken = async (refreshToken: string) => {
   };
 };
 
-const resetPassword = async (
+const resetPassword = async () =>
+  // oldPassword: string,
+  // newPassword: string,
+  // decodedToken: JwtPayload
+  {
+    // const user = await User.findById(decodedToken.userId);
+    // const isOldPasswordMatch = await bcryptjs.compare(
+    //   oldPassword,
+    //   user!.password as string
+    // );
+    // if (!isOldPasswordMatch) {
+    //   throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not match");
+    // }
+
+    // user!.password = await bcryptjs.hash(
+    //   newPassword,
+    //   Number(envVars.BCRYPT_SALT_ROUND)
+    // );
+
+    // user?.save();
+
+    return {};
+  };
+
+const setPassword = async (userId: string, plainPassword: string) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new AppError(404, "User not Found");
+  }
+
+  if (
+    user.password &&
+    user.auths.some((providerObject) => providerObject.provider === "google")
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "You have already set your password. Now you can change the password from your profile update."
+    );
+  }
+
+  const hashedPassword = await bcryptjs.hash(
+    plainPassword,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
+
+  const credentialProvider: IAuthProvider = {
+    provider: "credentials",
+    providerID: user.email,
+  };
+
+  const auths: IAuthProvider[] = [...user.auths, credentialProvider];
+
+  user.password = hashedPassword;
+
+  user.auths = auths;
+
+  await user.save()
+};
+
+const changePassword = async (
   oldPassword: string,
   newPassword: string,
   decodedToken: JwtPayload
@@ -103,4 +162,6 @@ export const AuthServices = {
   // credentialsLogin,
   getNewAccessToken,
   resetPassword,
+  setPassword,
+  changePassword,
 };
